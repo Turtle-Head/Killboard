6850ed539861ba18e14bd85890f1be8cfa2db940
Turtle-Head <djinn.jecture@gmail.com>
2015-11-18 16:15:12 -0600

    Added counter for involved pilots, removed HR between attackers and victim

--- js/script.js
+++ js/script.js
@@ -48,20 +48,21 @@ function loadData() {
               "corpKB": 'http://zkillboard.com/corporation/' + articleStr.attackers[atk].corporationID + '/'
             };
           }
-          var formAtk = '<div class="dat">';
+          var formAtk = '<div class="dat">Involved: '+ (articleStr.attackers.length) + '<br>';
           for(var fin=0; fin < atkCorp.length; fin++){
             formAtk+='<img src="'+ atkCorp[fin].shipPic + '"><img src="'+ atkCorp[fin].wepPic + '"><a href="' + atkCorp[fin].corpKB + '"><img src="' + atkCorp[fin].corpPic + '" alt="' + atkCorp[fin].corp + '"></a>  ' /*+ atkCorp[fin].corp*/ + '<a href="' + atkCorp[fin].pilotkb + '"><img src="'+ atkCorp[fin].pilotP +'">  ' + atkCorp[fin].pilot + '</a><br>';
           }
           formAtk+='</div>';
           var vicCorpKB = 'http://zkillboard.com/corporation/' + articleStr.victim.corporationID + '/';
           var value = Number(articleStr.zkb.totalValue).toLocaleString('en');
-          var formISKP ='<div class="dat"><div class="dat2">ISK Value: <div class="num">'+ value + '</div></div><div class="dat2" width="33%"> Points: ' + articleStr.zkb.points + '</div></div>';
+          var formISKP ='<div class="dat"><div class="dat2">ISK Value: <div class="num">'+ value + '</div></div><div class="dat2"> Points: ' + articleStr.zkb.points + '</div></div>';
           var vicCorpUrl = 'http://imageserver.eveonline.com/Corporation/' + articleStr.victim.corporationID + '_128.png';
           var vicPic = 'http://imageserver.eveonline.com/Character/' + articleStr.victim.characterID + '_128.jpg';
+          var killOutput = '<div class="image"><a href="' + url + '"><img src="' + shipPic + '"><img src="'+ vicPic +'"></a><a href="' + vicCorpKB + '"><img src="' + vicCorpUrl + '"></div><div class="ids"><a href="' + url + '">' + articleStr.victim.characterName + '</a><br> Corp: <a href="' + vicCorpKB + '">' + articleStr.victim.corporationName + '</a>' + formISKP + '</div>' + '<div class="attackers">' + formAtk +'</li><hr>';
           if(articleStr.victim.corporationID == corporation){
-            $resElem.append('<li class="loss"><div class="image"><a href="' + url + '"><img src="' + shipPic + '"><img src="'+ vicPic +'"></a><a href="' + vicCorpKB + '"><img src="' + vicCorpUrl + '"></div><div class="ids"><a href="' + url + '">' + articleStr.victim.characterName + '</a><br> Corp: <a href="' + vicCorpKB + '">' + articleStr.victim.corporationName + '</a>' + formISKP + '</div>' + '<hr><div class="attackers">' + formAtk +'</li><hr>');
+            $resElem.append('<li class="loss">' + killOutput);
           } else {
-            $resElem.append('<li class="kill"><div class="image"><img src="' + shipPic + '"><img src="'+ vicPic +'"><img src="' + vicCorpUrl + '"></div><div class="ids"><a href="' + url + '">' + articleStr.victim.characterName + '</a><br> Corp: ' + articleStr.victim.corporationName + formISKP +'</div>'+ '<hr><div class="attackers">' + formAtk + '</li><hr>');
+            $resElem.append('<li class="kill">' + killOutput);
           }
         }
         $resElem.append('</ul>');
